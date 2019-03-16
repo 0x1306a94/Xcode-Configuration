14c087a2922181aa5e7717e8b12e2a51ffad1920
0x1306a94 <0x1306a94@gmail.com>
2019-03-16 16:55:39 +0800

    添加Xcode 文件模板

---
+++ File Templates/Custom/Model.xctemplate/NSObjectSwift/___FILEBASENAME___.swift
@@ -0,0 +1,36 @@
+//___FILEHEADER___
+
+/* System */
+import Foundation
+
+/* ViewController */
+
+/* View */
+
+/* Model */
+
+/* Util */
+
+/* NetWork InterFace */
+
+/* Vender */
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+
+	
+	#if DEBUG
+    deinit {
+        print("[\(type(of: self)) deinit]")
+    }
+    #endif
+}
+
+//MARK: - private method
+fileprivate extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+}
+
+//MARK: - public method
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+}
---
+++ File Templates/Custom/UIViewController.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,90 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+/* System */
+
+/* ViewController */
+
+/* View */
+
+/* Model */
+
+/* Util */
+
+/* NetWork InterFace */
+
+/* Vender */
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#if DEBUG
+- (void)dealloc {
+    NSLog(@"[%@ dealloc]", NSStringFromClass(self.class));
+}
+#endif
+
+#pragma mark - life cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self commonInit];
+}
+
+/*
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+}
+*/
+
+#pragma mark - initial Methods
+- (void)commonInit {
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    
+}
+
+#pragma mark - private method
+
+
+#pragma mark - public Method
+
+
+#pragma mark - event response
+
+
+#pragma mark - UITableViewDelegate
+
+
+#pragma mark - UITableViewDataSource
+//...(多个代理方法依次往下写)
+
+#pragma mark - getters and setters
+
+@end
